986bdfa417c083d497989139e78bba59d84833cc
Re <hikarisakata91@gmail.com>
2022-09-30 14:54:37 +0800

    '.'

--- config/config.js
+++ config/config.js
@@ -1,10 +1,16 @@
+const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_USERNAME = process.env.DB_USERNAME || "root";
+const DB_PASSWORD = process.env.DB_PASSWORD || "";
+const DB_NAME = process.env.DB_NAME || "noteApi";
+const DB_DIALECT = process.env.DB_DIALECT || "mysql";
+
 const config = {
     "development": {
-      "username": "root",
-      "password": "",
-      "database": "noteApi",
-      "host": "localhost",
-      "dialect": "mysql"
+      "username": DB_USERNAME,
+      "password": DB_PASSWORD,
+      "database": DB_NAME,
+      "host": DB_HOST,
+      "dialect": DB_DIALECT
     },
     "test": {
       "username": "root",
